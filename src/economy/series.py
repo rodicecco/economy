c4bcff670b11535cae05a94bbf06d1ab0eab0ac0
Victor Rodriguez <rodicecco@outlook.com>
2026-03-12 14:42:00 -0400

    Changed size of charts

--- src/economy/series.py
+++ src/economy/series.py
@@ -258,7 +258,9 @@ class Series(Transformations):
             y=-0.2,               # Position it below the x-axis (negative value)
             xanchor="center",     # Center the legend horizontally
             x=0.5),   
-            height=800, width=1200, template="plotly_white", hovermode="x")
+            #height=800, 
+            #width=1200, 
+            template="plotly_white", hovermode="x")
 
         return fig
     
